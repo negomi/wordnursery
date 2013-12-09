f29abe58d462b0f795fa33dc6d53592152ee7242
Imogen <imogen.wentworth@gmail.com>
2013-12-08 19:41:06 -0800

    Add :remember_me to attr_accessible

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :password, :password_confirmation, :remember_me
 
   has_many :lists, dependent: :destroy
 end
