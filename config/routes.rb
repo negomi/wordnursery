693e8880bdf2a54681b4c6e0ae625a3487a92d52
Imogen <imogen.wentworth@gmail.com>
2013-12-09 14:07:50 -0800

    Add Devise user scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Wordnursery::Application.routes.draw do
   devise_for :users, :controllers => {:registrations => "users"}
 
+  devise_scope :user do
+    get "/users/sign_out" => "devise/sessions#destroy"
+  end
+
   root to: "home#index"
 
   resources :words
