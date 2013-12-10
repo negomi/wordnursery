feba57fd8a6105e04f16d7d6b44c9a29e73dc1ae
Imogen <imogen.wentworth@gmail.com>
2013-12-10 13:06:19 -0800

    Tidy up whitespace

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -5,8 +5,10 @@ describe "Pages" do
   describe "Home" do
 
     it "should have the content 'wordnursery'" do
+
       visit root_path
       expect(page).to have_content('wordnursery')
     end
+
   end
 end
