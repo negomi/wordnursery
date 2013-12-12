fa4e41345adc18dc2b04342511b29c5d6afd71a0
Imogen <imogen.wentworth@gmail.com>
2013-12-11 23:42:25 -0800

    Shorten create method

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -9,14 +9,18 @@ class WordsController < ApplicationController
   end
 
   def create
-    user = current_user
-    user.lists[0].words.create(
+    current_user.lists[0].words.create(
       name: params[:word],
       pronunciation: params[:pronunciation],
       definition: params[:definitions],
       attribution: params[:attribution]
       )
-    redirect_to user_lists_path(user.id)
+    redirect_to user_lists_path(current_user.id)
+  end
+
+  def change_word_list
+
+
   end
 
   def edit
