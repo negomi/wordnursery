4a3d5abee1ecbeb975543ff5f8e34a1fdbda83ac
Imogen <imogen.wentworth@gmail.com>
2013-12-11 23:38:29 -0800

    Add redirect path after user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,6 @@ class UsersController < Devise::RegistrationsController
     user.lists.create(name: "kindergarten")
     user.lists.create(name: "school")
     user.lists.create(name: "graduated")
+    redirect_to user_lists_path
   end
 end
